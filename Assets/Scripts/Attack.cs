c5a2ffc3efc37a3f1c9dbd63ffda90e6fa9e9071
Oh-Hi-Angeu <reber379@gmail.com>
2024-12-19 10:34:33 +0100

    Initial Commit

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Se om den kan bli träffad
+        Damagable damagable = collision.GetComponet(); 
+    }
+}
